0c4197bf8f39bc5519eaecc2cf25ca9d8b6a3a15
GuDahoon <iamkdh1@naver.com>
2023-11-09 17:21:27 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,68 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./routes/home";
+import Layout from "./components/layout";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/loading-screen";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />
+      }
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />
+  }
+])
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color: white;
+    font-family: "Helvetica", "Arial", sans-serif;
+
+  }
+`;
+
+
+
 function App() {
+  const[isLoading, setLoading] = useState(true);
+  const init = async() => {
+    // wait for firebase
+    // setTimeout(() => setIsLoading(false), 1000); -> for test loading
+    setLoading(false);
+  }
+  useEffect(() => {
+    init();
+  }, [])
   return (
     <>
-      
+      <GlobalStyles />
+      {isLoading ? <LoadingScreen/> : <RouterProvider router={router} />}
     </>
   )
 }
---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBAOATQ0vSh8Whwc8xlHLvajCXXRKH20UY",
+  authDomain: "nwitter-reloaded-a3cb6.firebaseapp.com",
+  projectId: "nwitter-reloaded-a3cb6",
+  storageBucket: "nwitter-reloaded-a3cb6.appspot.com",
+  messagingSenderId: "879438411146",
+  appId: "1:879438411146:web:e79768d0cd91f4b03d0473"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
